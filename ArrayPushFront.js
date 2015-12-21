8c7cb95c943192b4e16d3d00c50dbd07678f0b76
Matt Yoshihara <mkyoshihara@gmail.com>
2015-12-21 00:04:00 -0800

    added pushFront function to Array prototype

--- ArrayPushFront.js
+++ ArrayPushFront.js
@@ -1,26 +1,46 @@
 // Given an array and additional value, insert this value at the beginning of the array.
 
+// pushFront as a function that is not part of the Array prototype
+// function pushFront(array, value) {
+// 	if (!Array.isArray(array)){
+// 		throw "1st Parameter not an array"
+// 	}
 
-function pushFront(array, value) {
-	if (!Array.isArray(array)){
-		throw "1st Parameter not an array"
-	}
+// 	if (value === undefined) {
+// 		throw "2nd Parameter undefined"
+// 	}
+// 	for (var i = array.length; i > 0; i--) {
+// 		array[i] = array[i-1];
+// 	}
+// 	array[0] = value;
+// }
 
-	if (value === undefined) {
-		throw "2nd Parameter undefined"
-	}
-	for (var i = array.length; i > 0; i--) {
+
+// try {
+// 	array = [1,2,3,4,5]
+// 	pushFront(array,6)
+// } catch(err) {
+// 	console.log("Error: ", err)
+// }
+
+// console.log("New array: ",array)
+
+
+// Added pushFront function to the prototype of the Array object
+Array.prototype.pushFront = function(value) {
+	for(var i = this.length; i>0; i--){
 		array[i] = array[i-1];
 	}
 	array[0] = value;
 }
 
+// Test cases
+array = [1,2,3,4,5];
+array.pushFront(0);
+array.pushFront("Hello");
+array.pushFront(false);
+array.pushFront(function(value){
+	console.log(value);
+});
 
-try {
-	array = [1,2,3,4,5]
-	pushFront(array)
-} catch(err) {
-	console.log("Error: ", err)
-}
-
-console.log("New array: ",array)
+console.log(array);
