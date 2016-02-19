6d6dbb29eec71a65809209ea323c1fb6b089a5d9
Matt Yoshihara <mkyoshihara@gmail.com>
2016-02-19 13:49:46 -0800

    added lengthOfLongestSubstring.Java

--- Java/Strings/lengthOfLongestSubstring.java
+++ Java/Strings/lengthOfLongestSubstring.java
@@ -1,4 +1,5 @@
-Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+// Given a string, find the length of the longest substring without repeating characters. 
+// For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
 
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
@@ -30,3 +31,6 @@ public class Solution {
         return max;
     }
 }
+
+// Testes at https://leetcode.com/problems/longest-substring-without-repeating-characters/
+// Completed with O(n) runtime, O(n) space
