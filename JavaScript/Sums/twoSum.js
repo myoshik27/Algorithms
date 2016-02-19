1d04a5500f041c93ec3f1cbf10a2bf0fc8b2d84e
Matt Yoshihara <mkyoshihara@gmail.com>
2016-02-18 23:23:55 -0800

    completed with O(n) runtime, O(n) Space

--- JavaScript/Sums/twoSum.js
+++ JavaScript/Sums/twoSum.js
@@ -1,5 +1,6 @@
 // Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
+// You may assume that nums is an array of integers
+// You may assume that target is an integer
 // You may assume that each input would have exactly one solution.
 
 // Example:
@@ -9,3 +10,25 @@
 // return [0, 1].
 
 
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var answer = [];
+    var dictionary = {};
+    for(var i=0; i<nums.length; i++){
+        if(dictionary[nums[i]] !== undefined){
+            answer[0] = dictionary[nums[i]];
+            answer[1] = i;
+            return answer;
+        }
+        dictionary[target-nums[i]] = i;
+    }
+    return answer;
+};
+
+
+// Tested on https://leetcode.com/problems/two-sum/
+// O(n) runtime, O(n) space
