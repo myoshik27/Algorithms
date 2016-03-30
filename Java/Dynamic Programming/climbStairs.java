5ced63aca71bbedb14d2c7112d234eaa8c473447
Matt Yoshihara <mkyoshihara@gmail.com>
2016-03-29 23:52:17 -0700

    added comments to climbStairs.java

--- Java/Dynamic Programming/climbStairs.java
+++ Java/Dynamic Programming/climbStairs.java
@@ -1,3 +1,10 @@
+/*
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+*/
+
+
 public class Solution {
     public int climbStairs(int n) {
         if(n == 0 || n == 1 || n == 2 || n == 3){
@@ -14,3 +21,6 @@ public class Solution {
         }
     }
 }
+
+// Tested at https://leetcode.com/problems/climbing-stairs/
+// Completed with O(n) time complexity and O(1) space complexity
