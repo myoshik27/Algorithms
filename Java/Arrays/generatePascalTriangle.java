46c41831495f825331650f51bb58bab3e93b7862
Matt Yoshihara <mkyoshihara@gmail.com>
2016-04-14 20:10:58 -0700

    generatePascalTriangle

---
+++ Java/Arrays/generatePascalTriangle.java
@@ -0,0 +1,32 @@
+/*
+Given numRows, generate the first numRows of Pascal's triangle.
+
+For example, given numRows = 5,
+Return
+
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+
+]
+
+*/
+
+
+public class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        ArrayList<List<Integer>> triangle = new ArrayList<List<Integer>>();
+        for(int i=0; i<numRows; i++){
+            ArrayList<Integer> row = new ArrayList<Integer>();
+            for(int j=0; j<=i; j++){
+                if(j == 0 || j == i) row.add(1);
+                else row.add(triangle.get(i-1).get(j-1) + triangle.get(i-1).get(j));
+            }
+            triangle.add(row);
+        }
+        return triangle;
+    }
+}
